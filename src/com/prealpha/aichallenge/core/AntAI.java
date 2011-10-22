1a0111c58e11d07adccfc6111dbebecd1bbd41b7
Meyer Kizner <meyer@pre-alpha.com>
2011-10-22 15:28:40 -0400

    Make it compile

--- src/com/prealpha/aichallenge/core/AntAI.java
+++ src/com/prealpha/aichallenge/core/AntAI.java
@@ -1,7 +1,7 @@
 package com.prealpha.aichallenge.core;
 
 import com.prealpha.aichallenge.MyBot;
-import com.prealpha.aichallenge.astar.AStarAgent;
+import com.prealpha.aichallenge.ai.AStarAgent;
 import com.prealpha.aichallenge.protocol.GameMap;
 import com.prealpha.aichallenge.protocol.Point;
 
@@ -9,34 +9,34 @@ public class AntAI {
 	private Point curPosition;
 	private Point projectedPosition;
 	private AStarAgent agent;
-	
+
 	private GameMap currentGameState;
 	private MyBot myBot;
-	
-	public AntAI(Point p,MyBot myBot){
-		this.curPosition=p;
-		this.myBot=myBot;
+
+	public AntAI(Point p, MyBot myBot) {
+		this.curPosition = p;
+		this.myBot = myBot;
 	}
-	
-	public void update(){
+
+	public void update() {
 		this.currentGameState = MyBot.getGm();
-		
-		//Check to see if the ant that this AI is controlling is actually still alive.
-		for(Point p:this.currentGameState.getMyAnts()){
+
+		// Check to see if the ant that this AI is controlling is actually still
+		// alive.
+		for (Point p : this.currentGameState.getMyAnts()) {
 			boolean isAlive = false;
-			if(p.equals(this.projectedPosition)){
+			if (p.equals(this.projectedPosition)) {
 				isAlive = true;
 				break;
 			}
-			if(!isAlive){
+			if (!isAlive) {
 				this.die();
 			}
 		}
-		
-		
+
 	}
-	
-	public void die(){
+
+	public void die() {
 		myBot.removeAntAI(this);
 	}
 }
