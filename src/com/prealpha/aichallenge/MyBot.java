a9d99d471942f68ea46f4670ce9075e945948205
Meyer Kizner <meyer@pre-alpha.com>
2011-10-22 16:16:27 -0400

    Rename Map back to GameMap so that we don't confuse it with java.util.Map

--- src/com/prealpha/aichallenge/MyBot.java
+++ src/com/prealpha/aichallenge/MyBot.java
@@ -8,7 +8,7 @@ import com.prealpha.aichallenge.core.AntAI;
 import com.prealpha.aichallenge.protocol.Aim;
 import com.prealpha.aichallenge.protocol.Bot;
 import com.prealpha.aichallenge.protocol.Game;
-import com.prealpha.aichallenge.protocol.Map;
+import com.prealpha.aichallenge.protocol.GameMap;
 import com.prealpha.aichallenge.protocol.Point;
 
 /**
@@ -29,7 +29,7 @@ public class MyBot extends Bot {
 	@Override
 	public void doTurn() {
 		Game game = getGame();
-		Map map = getMap();
+		GameMap map = getMap();
 		for (Point myAnt : map.getMyAnts()) {
 			for (Aim direction : Aim.values()) {
 				if (map.getIlk(myAnt, direction).isPassable()) {
