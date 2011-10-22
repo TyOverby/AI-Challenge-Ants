44dffd08dbfc16e00addd98dcb3b9afb88d2692d
Meyer Kizner <meyer@pre-alpha.com>
2011-10-22 19:40:08 -0400

    Documentation and such for Path

--- src/com/prealpha/aichallenge/ai/Path.java
+++ src/com/prealpha/aichallenge/ai/Path.java
@@ -9,6 +9,13 @@ import java.util.Set;
 import com.prealpha.aichallenge.protocol.GameMap;
 import com.prealpha.aichallenge.protocol.Point;
 
+/**
+ * A sequence of {@link Point} instances in which each successive point is
+ * directly adjacent to the last. Paths are immutable.
+ * 
+ * @author Meyer Kizner
+ * 
+ */
 public final class Path {
 	private final GameMap map;
 
@@ -21,17 +28,21 @@ public final class Path {
 	public Path(GameMap map, List<Point> nodes) {
 		this.map = map;
 		this.nodes = new ArrayList<Point>(nodes);
+
+		// check that all points are adjacent
+		Point last = this.nodes.get(0);
+		for (int i = 1; i < this.nodes.size(); i++) {
+			Point current = this.nodes.get(i);
+			if (this.map.getDistance(last, current) != 1.0) {
+				throw new IllegalArgumentException();
+			}
+			last = current;
+		}
 	}
 
-	public Set<Path> getChildren() {
-		Set<Path> toReturn = new HashSet<Path>();
-		Set<Point> adjacent = map.getAdjacent(getHead());
-		for (Point point : adjacent) {
-			if (map.getIlk(point).isPassable() && !nodes.contains(point)) {
-				toReturn.add(branch(point));
-			}
-		}
-		return toReturn;
+	private Path(Path template) {
+		map = template.map;
+		nodes = new ArrayList<Point>(template.nodes);
 	}
 
 	public Point getHead() {
@@ -42,21 +53,55 @@ public final class Path {
 		if (map.getDistance(getHead(), target) != 1.0) {
 			throw new IllegalArgumentException();
 		}
-		Path toReturn = new Path(map, nodes);
-		toReturn.nodes.add(target);
-		return toReturn;
+		Path branch = new Path(this);
+		branch.nodes.add(target);
+		return branch;
 	}
 
+	public Set<Path> getChildren() {
+		Set<Path> children = new HashSet<Path>(4);
+		Set<Point> adjacent = map.getAdjacent(getHead());
+		for (Point point : adjacent) {
+			if (map.getIlk(point).isPassable() && !nodes.contains(point)) {
+				children.add(branch(point));
+			}
+		}
+		return children;
+	}
+
+	/**
+	 * Returns the length of this path. In A*, this is g(head).
+	 * 
+	 * @return the length of this path
+	 */
 	public int getLength() {
 		return nodes.size() - 1;
 	}
 
-	public double getHeuristicDistance(Point endPoint) {
-		Point turn = new Point(getHead().getRow(), endPoint.getCol());
-		return map.getDistance(getHead(), turn)
-				+ map.getDistance(turn, endPoint);
+	/**
+	 * Returns the minimum Manhattan distance from the head of the path to the
+	 * specified end point. This is used as the heuristic, or h(head) in A*. The
+	 * actual distance may be longer if there are obstacles present between the
+	 * head and the end point.
+	 * 
+	 * @param end
+	 *            the target point
+	 * @return the minimum Manhattan distance from the head to {@code end}
+	 */
+	public double getHeuristicDistance(Point end) {
+		Point turn = new Point(getHead().getRow(), end.getCol());
+		return map.getDistance(getHead(), turn) + map.getDistance(turn, end);
 	}
 
+	/**
+	 * Returns the total estimated distance along this path and then to the end
+	 * point. Past the head, distances are estimated using
+	 * {@link #getHeuristicDistance(Point)}. In A*, this is f(head).
+	 * 
+	 * @param end
+	 *            the target point
+	 * @return the total estimated distance along the path to {@code end}
+	 */
 	public double getTotalDistance(Point end) {
 		return getLength() + getHeuristicDistance(end);
 	}
