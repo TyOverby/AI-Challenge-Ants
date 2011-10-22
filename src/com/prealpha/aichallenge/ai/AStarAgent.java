5db257cfb5b86740154abd306ee8510d4867b427
Meyer Kizner <meyer@pre-alpha.com>
2011-10-22 17:01:53 -0400

    Formatting

--- src/com/prealpha/aichallenge/ai/AStarAgent.java
+++ src/com/prealpha/aichallenge/ai/AStarAgent.java
@@ -15,32 +15,31 @@ public class AStarAgent {
 
 	private final Set<Point> exploredPoints = new HashSet<Point>();
 
-	public AStarAgent(GameMap map,Point start, Point end) {
+	public AStarAgent(GameMap map, Point start, Point end) {
 		this.start = start;
 		this.end = end;
 
-		paths.add(new Path(map,this.start));
+		paths.add(new Path(map, this.start));
 		exploredPoints.add(this.start);
-	}	
+	}
 
-	public Path findCurrentMin(){
-
+	public Path findCurrentMin() {
 		// Find the smallest current path
 		Path curMin = paths.get(0);
-		for(Path p:paths){
+		for (Path p : paths) {
 			// see if it is the smallest
-			if(p.getTotalDist(end)<curMin.getTotalDist(end)){
+			if (p.getTotalDist(end) < curMin.getTotalDist(end)) {
 				curMin = p;
 			}
 		}
 
-		return curMin;		
+		return curMin;
 	}
 
-	public void advance(){
+	public void advance() {
 		Path minPath = findCurrentMin();
 
-		// Add all of its children 
+		// Add all of its children
 		paths.addAll(minPath.getNewPathsFromHead());
 		paths.remove(minPath);
 	}
@@ -48,14 +47,13 @@ public class AStarAgent {
 	public Path getSmallestPath() {
 		Path minPath = findCurrentMin();
 
-		// Add all of its children 
+		// Add all of its children
 		paths.addAll(minPath.getNewPathsFromHead());
 		paths.remove(minPath);
 
-		if(minPath.getHead().equals(end)){
+		if (minPath.getHead().equals(end)) {
 			return minPath;
-		}
-		else{
+		} else {
 			return getSmallestPath();
 		}
 	}
