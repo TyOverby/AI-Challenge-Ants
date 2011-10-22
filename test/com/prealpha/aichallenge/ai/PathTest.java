2a8991b2f51aacfb7b3be697f0739c3e8c67e49b
Meyer Kizner <meyer@pre-alpha.com>
2011-10-22 16:05:50 -0400

    Paths require a map parameter, and pass unit tests

--- test/com/prealpha/aichallenge/ai/PathTest.java
+++ test/com/prealpha/aichallenge/ai/PathTest.java
@@ -1,24 +1,45 @@
 package com.prealpha.aichallenge.ai;
 
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.prealpha.aichallenge.protocol.Map;
 import com.prealpha.aichallenge.protocol.Point;
 
-public class PathTest {
+public final class PathTest {
+	private static final int ROWS = 20;
+
+	private static final int COLS = 20;
+
+	private Map map;
+
+	@Before
+	public void setUp() throws InstantiationException, IllegalAccessException,
+			InvocationTargetException {
+		// since I don't want to make this constructor public
+		Constructor<?> constructor = Map.class.getDeclaredConstructors()[0];
+		constructor.setAccessible(true);
+		map = (Map) constructor.newInstance(ROWS, COLS);
+	}
+
 	@Test
 	public void testPathDistance() {
 		Point start = new Point(0, 0);
 		Point end = new Point(0, 5);
 
-		Path path = new Path(start);
-		assert (path.getTravledDistance() == 0);
-		assert (path.getEstimatedRemainingDistance(end) == 5);
-		assert (path.getTotalDist(end) == 5);
+		Path path = new Path(map, start);
+		assertEquals(0, path.getTraveledDistance());
+		assertEquals(5, path.getEstimatedRemainingDistance(end), 0.001);
+		assertEquals(5, path.getTotalDist(end), 0.001);
 	}
 
 	@Test
@@ -31,20 +52,20 @@ public class PathTest {
 		pathset.add(start);
 		pathset.add(next);
 
-		Path path = new Path(pathset);
+		Path path = new Path(map, pathset);
 
-		assert (path.getTravledDistance() == 1);
-		assert (path.getEstimatedRemainingDistance(end) == 6);
-		assert (path.getTotalDist(end) == 7);
+		assertEquals(1, path.getTraveledDistance());
+		assertEquals(6, path.getEstimatedRemainingDistance(end), 0.001);
+		assertEquals(7, path.getTotalDist(end), 0.001);
 	}
 
 	@Test
 	public void testGetHead() {
 		Point start = new Point(0, 0);
 
-		Path path = new Path(start);
+		Path path = new Path(map, start);
 
-		assert (path.getHead().equals(start));
+		assertEquals(start, path.getHead());
 	}
 
 	@Test
@@ -52,23 +73,23 @@ public class PathTest {
 		Point start = new Point(0, 0);
 		Point next = new Point(0, 1);
 
-		Path path = new Path(start);
+		Path path = new Path(map, start);
 		path.addPoint(next);
 
-		assert (path.getHead().equals(next));
+		assertEquals(next, path.getHead());
 	}
 
 	@Test
 	public void testPathBranch() {
 		Point start = new Point(0, 0);
-		Path curPath = new Path(start);
+		Path curPath = new Path(map, start);
 
 		Set<Path> newPaths = new HashSet<Path>();
-		newPaths.add(new Path(start, new Point(0, 1)));
-		newPaths.add(new Path(start, new Point(0, -1)));
-		newPaths.add(new Path(start, new Point(1, 0)));
-		newPaths.add(new Path(start, new Point(-1, 0)));
+		newPaths.add(new Path(map, start, new Point(0, 1)));
+		newPaths.add(new Path(map, start, new Point(0, COLS - 1)));
+		newPaths.add(new Path(map, start, new Point(1, 0)));
+		newPaths.add(new Path(map, start, new Point(ROWS - 1, 0)));
 
-		assert (curPath.getNewPathsFromHead().equals(newPaths));
+		assertEquals(newPaths, curPath.getNewPathsFromHead());
 	}
 }
